d5f5dcf401aab4b765c2d5ab267d120d8388fe9b
achieve_dream <wulei7217@gmail.com>
2023-03-23 11:15:28 +0800

    修改了数据集的参数, 预测成功

--- model.py
+++ model.py
@@ -10,26 +10,27 @@ import torch.nn as nn
 
 class LeNet(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=3)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc3 = nn.Linear(in_features=16 * 6 * 6, out_features=120)
-        self.fc4 = nn.Linear(in_features=120, out_features=84)
-        self.fc5 = nn.Linear(in_features=84, out_features=10)
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool1(torch.relu(self.conv1(x)))
         x = self.pool2(torch.relu(self.conv2(x)))
-        x = x.view(x.size(0), -1)
-        x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
-        return self.fc5(x)
+        x = x.view(-1, 16 * 4 * 4)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 
 if __name__ == '__main__':
     model = LeNet()
-    ret = model(torch.randn(1, 1, 32, 32))
+    ret = model(torch.randn(1, 1, 28, 28))
     print(ret.shape)
     print(ret)
